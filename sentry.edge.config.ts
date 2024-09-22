5a4b1f363f4d80f8a5dff73e0a7cd407c1730770
Ayman Hawi <118613736+aymanhawi00@users.noreply.github.com>
2024-09-22 16:52:16 +0300

    Add files via upload

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,9 +6,9 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://854f6db848cacb50904b2382acf4f82a@o4507996144992256.ingest.us.sentry.io/4507996148137984",
+  dsn: "https://e300b41df2ffb6d4592972400a25c0d3@o4506876178464768.ingest.us.sentry.io/4507159179034624",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
